d61d54fb55c9ce957e9f47b41a0c6e78392a1f2d
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-06-29 09:55:43 -0700

    working with basic CRUD

--- src/app.js
+++ src/app.js
@@ -1,22 +1,59 @@
-import React from 'react';
-import Header from './components/header/header';
-import Form from './components/form/form';
-import Response from './components/response/response';
-import Footer from './components/footer/footer';
+import React from "react";
+import ReactJSON from "react-json-view";
+import superagent from "superagent";
 
-import('./app.scss');
+import Header from "./components/header/header";
+import Form from "./components/form/form";
+import Footer from "./components/footer/footer";
 
-export default function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <section>
-          <Form />
-          <Response />
-        </section>
-      </main>
-      <Footer />
-    </>
-  );
+import("./app.scss");
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      URL: "",
+      method: "",
+      response: {
+        Headers: {},
+        Response: {}
+      }
+    };
+  }
+
+  handleURL = event => {
+    this.setState({ URL: event.target.value });
+  };
+
+  handleMethod = event => {
+    this.setState({ method: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const result = await superagent(this.state.method, this.state.URL);
+    // console.log(result.body.results);
+    this.setState({ response: result });
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <main>
+          <section>
+            <Form
+              handleURL={this.handleURL}
+              handleMethod={this.handleMethod}
+              handleSubmit={this.handleSubmit}
+            />
+            <div id="json">
+              <ReactJSON src={this.state.response} />
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </>
+    );
+  }
 }
